b445d3962c6e0a4b14b954edfd99ee1af5811c3d
Avigail <royzenbe@mail.yu.edu>
2021-01-11 16:36:39 -0500

    updated output

--- fsm.c
+++ fsm.c
@@ -33,7 +33,6 @@ int countTransitions(FILE *defs){
 int main(int argc, char *argv[]){  
   FILE *inputf = fopen(argv[2], "r");     //argv[2]: input file
   int lines = countInputs(inputf);
-  printf("this is how many lines are in the input file: %d\n", lines); //comment out later
   
 
   FILE *defs = fopen(argv[1], "r");       //argv[1]: fsm definition
@@ -44,15 +43,14 @@ int main(int argc, char *argv[]){
 
   printf("processing FSM definition file %s\n", argv[1]);
   printf("processing FSM inputs file %s\n", argv[2]);
-  //test to see if both files were able to be opened
 
   // Load data from FSM definition into 3 arrays:
-  int currentStates[trans]; 
+  int currentStates[trans];
   char inputs[trans];
   int nextStates[trans];
 
   // Keeping track of what states we're on:
-  int cur;
+  int cur=0;
   char input;
   int next;
 
@@ -63,28 +61,34 @@ int main(int argc, char *argv[]){
   int step = 0; // how many steps we have done
   FILE *inp = fopen(argv[2], "r");       //argv[2]: inputs
   FILE *defin = fopen(argv[1], "r");      //argv[1]: fsm definition
-
-  while ((pattern= fscanf(inp, "%c\n", &input)) != EOF) {
-    while((pattern2 = fscanf(defin, "%d:%c>%d", &cur, &input, &next)) != EOF){
-      currentStates[index]=cur;
-      inputs[index]=input;
-      nextStates[index]=next;
-      index++;
-      if (pattern && pattern2) {
-	for (int i = 0; i <trans; i++) {
-	  if (currentStates[i] == cur && inputs[i] == input) {
-	    next = nextStates[i];
-	    break;
-	  }
-	}
-	printf("at step %d, input %c transitions FSM from state %d to state %d\n", step, input, cur, next);
-	cur = next;
-	step += 1;
+  while((pattern = fscanf(defin, "%d:%c>%d", &cur, &input, &next)) != EOF){
+      if (pattern){
+	 currentStates[index]=cur;
+	 inputs[index]=input;
+	 nextStates[index]=next;
+	 index++;
       }
     }
+  fclose(defin);
+  int start = 0;
+  
+  while ((pattern2= fscanf(inp, "%c\n", &input)) != EOF) {
+    if (pattern2) {
+      for (int i = 0; i <trans; i++)
+        if (currentStates[i] == cur && inputs[i] == input) {
+	   next = nextStates[i];
+	   break;
+	 }
+      }
+    if (step == 0){ // always start at state 0
+      cur = 0;  
+      next = 1;
+    }
+    printf("at step %d, input %c transitions FSM from state %d to state %d\n", step, input, cur, next);
+    cur = next;
+    step += 1;
   }
   fclose(inp);
-  fclose(defin);
-  printf("you did it AGAIN, avigail\n");
   printf("after %d steps, state machine finished successfully at state %d\n", step, cur);
+  return (0);
 }
